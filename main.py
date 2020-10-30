7cbd5c41f6a8a617cd48903f821e70d3cc1f6260
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-30 19:15:41 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import pandas as pd
+
+# This reads in the data from the movie_data.txt file
+movies_df = pd.read_csv("movie_data.txt", index_col="Title")
+
+# You need to set the following variables using the information in the data
+# frame and the commands that are explained in the panel on the right.
+split_runtime = 
+prometheus_metascore =
+arrival_rating = 
+moana_takings =
+colossal_votes =
